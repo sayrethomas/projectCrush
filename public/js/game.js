36ebf445694f6917499f777af7e9e56626b8b846
Sayre Thomas <sayrethomasis@gmail.com>
2018-02-14 01:48:03 -0700

    Add: Moving Char

--- public/js/game.js
+++ public/js/game.js
@@ -1,36 +1,143 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
+// A cross-browser requestAnimationFrame
+// See https://hacks.mozilla.org/2011/08/animating-with-javascript-from-setinterval-to-requestanimationframe/
+var requestAnimFrame = (function(){
+    return window.requestAnimationFrame       ||
+        window.webkitRequestAnimationFrame ||
+        window.mozRequestAnimationFrame    ||
+        window.oRequestAnimationFrame      ||
+        window.msRequestAnimationFrame     ||
+        function(callback){
+            window.setTimeout(callback, 1000 / 60);
+        };
+})();
 
 // Create the canvas
 var canvas = document.createElement("canvas");
 var ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 450;
+
 document.body.appendChild(canvas);
 
-// Background image
-var bgReady = false;
-var bgImage = new Image();
-bgImage.onload = function () {
-	bgReady = true;
-};
-bgImage.src = "images/background.svg";
+// The main game loop
+var lastTime;
+function main() {
+    var now = Date.now();
+    var dt = (now - lastTime) / 1000.0;
 
-//render function
-var render = function() {
-    if(bgReady){
-        ctx.drawImage(bgImage, 0, 0);
-    }
-    requestAnimationFrame(main);
-};
-
-var main = function() {
+    update(dt);
     render();
+
+    lastTime = now;
+    requestAnimFrame(main);
 };
 
-var w = window;
-requestAnimationFrame = w.requestAnimationFrame || w.webkitRequestAnimationFrame || w.msRequestAnimationFrame || w.mozRequestAnimationFrame;
-main();
+function init() {
+
+    lastTime = Date.now();
+    main();
+}
+
+resources.load([
+    'img/character.png'
+]);
+resources.onReady(init);
+
+// Game state
+var player = {
+    pos: [0, 0],
+    sprite: new Sprite('img/character.png', [0, 0], [40, 40])
+};
+
+var gameTime = 0;
+
+
+
+
+// Speed in pixels per second
+var playerSpeed = 200;
+
+// Update game objects
+function update(dt) {
+    gameTime += dt;
+
+    handleInput(dt);
+    updateEntities(dt);
+
+    checkCollisions();
+
+};
+
+function handleInput(dt) {
+	var x = player.pos[0] + player.sprite.size[0] / 2;
+    var y = player.pos[1] + player.sprite.size[1] / 2;
+		
+    if(input.isDown('DOWN') || input.isDown('s')) {
+        player.pos[1] += playerSpeed * dt;
+    }
+
+    if(input.isDown('UP') || input.isDown('w')) {
+        player.pos[1] -= playerSpeed * dt;
+    }
+
+    if(input.isDown('LEFT') || input.isDown('a')) {
+        player.pos[0] -= playerSpeed * dt;
+    }
+
+    if(input.isDown('RIGHT') || input.isDown('d')) {
+        player.pos[0] += playerSpeed * dt;
+    }
+
+        
+}
+
+function updateEntities(dt) {
+    // Update the player sprite animation
+    player.sprite.update(dt);
+
+    
+
+}
+
+// Collisions
+function checkCollisions() {
+    checkPlayerBounds();
+    
+   
+}
+
+function checkPlayerBounds() {
+    // Check bounds
+    if(player.pos[0] < 0) {
+        player.pos[0] = 0;
+    }
+    else if(player.pos[0] > canvas.width - player.sprite.size[0]) {
+        player.pos[0] = canvas.width - player.sprite.size[0];
+    }
+
+    if(player.pos[1] < 0) {
+        player.pos[1] = 0;
+    }
+    else if(player.pos[1] > canvas.height - player.sprite.size[1]) {
+        player.pos[1] = canvas.height - player.sprite.size[1];
+    }
+}
+var background = new Image();
+background.src = 'img/background.svg';
+// Draw everything
+function render() {
+    //ctx.fillColor = "000000";
+    //ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.drawImage(background, 0, 0)
+ 
+    renderEntity(player);
+};
+
+function renderEntity(entity) {
+    ctx.save();
+    ctx.translate(entity.pos[0], entity.pos[1]);
+    entity.sprite.render(ctx);
+    ctx.restore();
+}
+
